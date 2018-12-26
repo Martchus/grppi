7fad1a2cd75964f4c5331d8f30a1ec7dc4c372a9
Martchus <martchus@gmx.net>
2018-12-26 23:34:33 +0100

    Don't use experimental namespace

--- include/native/parallel_execution_native.h
+++ include/native/parallel_execution_native.h
@@ -28,7 +28,7 @@
 #include <vector>
 #include <type_traits>
 #include <tuple>
-#include <experimental/optional>
+#include <optional>
 #include <sstream>
 #include <cstdlib>
 #include <cstring>
@@ -1055,7 +1055,6 @@ void parallel_execution_native::do_pipeline(Inqueue & input_queue, Transformer &
       mpmc_queue<output_type> & output_queue) const
 {
   using namespace std;
-  using namespace experimental;
 
   using output_item_value_type = typename output_type::first_type::value_type;
   for (;;) {
@@ -1077,7 +1076,6 @@ void parallel_execution_native::do_pipeline(
     OtherTransformers && ... other_transform_ops) const
 {
   using namespace std;
-  using namespace experimental;
 
   using input_item_type = typename Queue::value_type;
   using input_item_value_type = typename input_item_type::first_type::value_type;
@@ -1140,13 +1138,12 @@ void parallel_execution_native::do_pipeline(Queue & input_queue,
     OtherTransformers &&... other_ops) const 
 {
   using namespace std;
-  using namespace experimental;
 
   using input_item_type = typename Queue::value_type;
   using input_item_value_type = typename input_item_type::first_type::value_type;
 
   using output_type = typename stage_return_type<input_item_value_type, Transformer>::type;
-  using output_optional_type = experimental::optional<output_type>;
+  using output_optional_type = optional<output_type>;
   using output_item_type = pair <output_optional_type, long> ;
 
   decltype(auto) output_queue =
@@ -1177,13 +1174,12 @@ void parallel_execution_native::do_pipeline(
     OtherTransformers && ... other_transform_ops) const
 {
   using namespace std;
-  using namespace experimental;
 
   using input_item_type = typename Queue::value_type;
   using input_item_value_type = typename input_item_type::first_type::value_type;
 
   using output_type = typename stage_return_type<input_item_value_type, FarmTransformer>::type;
-  using output_optional_type = experimental::optional<output_type>;
+  using output_optional_type = optional<output_type>;
   using output_item_type = pair <output_optional_type, long> ;
 
   decltype(auto) output_queue = 
@@ -1220,7 +1216,6 @@ void parallel_execution_native::do_pipeline(
     OtherTransformers && ... other_transform_ops) const
 {
   using namespace std;
-  using namespace experimental;
 
   using input_item_type = typename Queue::value_type;
   using input_value_type = typename input_item_type::first_type;
@@ -1313,7 +1308,6 @@ void parallel_execution_native::do_pipeline(
     OtherTransformers && ... other_transform_ops) const
 {
   using namespace std;
-  using namespace experimental;
 
   using output_item_value_type = optional<decay_t<Identity>>;
   using output_item_type = pair<output_item_value_type,long>;
@@ -1351,7 +1345,6 @@ void parallel_execution_native::do_pipeline(
     OtherTransformers && ... other_transform_ops) const
 {
   using namespace std;
-  using namespace experimental;
 
   using input_item_type = typename decay_t<Queue>::value_type;
 
